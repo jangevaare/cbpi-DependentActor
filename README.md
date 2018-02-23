0ad1189253e0f1a42ce3fd2da04db38947c0be57
Justin Angevaare <jangevaa@uoguelph.ca>
2018-02-23 18:48:12 -0500

    make it work

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# cbpi-mutually-exclusive-actors
-A CraftBeerPi 3 plugin which provides mutually exclusive actors
+# cbpi-dependent-actors
+A CraftBeerPi 3 plugin which provides dependent actors. Two new actor types are
+provided:
+* Actor with Restriction
+* Actor with Prerequisite
+
+This plugin is not a substitute for properly rated hardware components and safety features.
+
+## Actor with Restriction
+This is an actor that can *NOT* be activated when it's restricting actor is already active. A notification message will be provided if activation of an actor has been prevented.
+
+## Actor with Prerequisite
+This is an actor that can *ONLY* be activated when it's prerequisite actor is already active. A notification message will be provided if activation of an actor has been prevented.
--- __init__.py
+++ __init__.py
@@ -1,47 +1,50 @@
 from modules import cbpi
 from modules.core.props import Property
 from modules.core.hardware import ActorBase
-from modules.core.hardware import SensorPassive
 
 @cbpi.actor
-class Actor_with_Restriction(ActorBase):
-    base = Property.Actor("Base Actor", description="Select the actor you would like to limit to when it's restriction is off")
-    restriction = Property.Actor("Actor Restriction", description="Select the actor that will prevents the base actor from becoming active, when it itself is active")
-    #
-    # def init(self):
-    #     self.base.hide == TRUE
+class DependentActor(ActorBase):
+    base = Property.Actor(label="Base Actor", description="Select the actor you would like to add a dependency to.")
+    dependency_type = Property.Select(label="Dependency Type", options=["Restriction", "Prerequisite"], description="Select the dependency type. With 'Restriction', the 'Actor Dependency' is required to be OFF in order to switch the 'Base Actor' ON. With 'Prerequisite', the 'Actor Dependency' is required to be ON in order to switch the 'Base Actor' ON.")
+    dependency = Property.Actor(label="Actor Dependency", description="Select the actor that the base actor will depend upon.")
+
+    def init(self):
+        # Make sure the Base Actor is off
+        self.api.switch_actor_off(int(self.base))
+        # Hide Base Actor
+        for idx, value in cbpi.cache["actors"].iteritems():
+            if idx == int(self.base):
+                value.hide = True
+
+        # Synchronize current power setting with the Base Actor
+        # TODO
 
     def set_power(self, power):
-        if self.exclusion.state == 0:
-            self.api_actor_power(self.base, power=power)
-        else:
-            self.notify(headline="Set power prevented", message="Could not set power to %s, since %s is currently on" % (self.base, self.exclusion), timeout=None)
-
-    def on(self, power=None):
-        if self.exclusion.state == 0:
-            self.api.switch_actor_on(self.base, power=power)
-        else:
-            self.notify(headline="Actor activation prevented", message="Could not turn %s on, since %s is currently on" % (self.base, self.exclusion), timeout=None)
+        self.api.actor_power(int(self.base), power=power)
 
     def off(self):
-        self.api.switch_actor_off(self.base)
-
-@cbpi.actor
-class Actor_with_Prerequisite(ActorBase):
-    base = Property.Actor("Base Actor", description="Select the actor you would like to limit to when it's prerequisite is on")
-    requisite = Property.Actor("Actor Prerequisite", description="Select the actor that enables the base actor to become active, when it itself is active")
-
-    def set_power(self, power):
-        if self.requisite.state == 1:
-            self.api_actor_power(self.base, power=power)
-        else:
-            self.notify(headline="Set power prevented", message="Could not set power to %s, since %s is currently off" % (self.base, self.exclusion), timeout=None)
+        self.api.switch_actor_off(int(self.base))
 
     def on(self, power=None):
-        if self.requisite.state == 1:
-            self.api.switch_actor_on(self.base, power=power)
-        else:
-            self.notify(headline="Actor activation prevented", message="Could not turn %s on, since %s is currently off" % (self.base, self.exclusion), timeout=None)
+        base_name = ""
+        dependency_name = ""
+        for idx, value in cbpi.cache["actors"].iteritems():
+            if idx == int(self.base):
+                base_name = value.name
+            if idx == int(self.dependency):
+                dependency_name = value.name
 
-    def off(self):
-        self.api.switch_actor_off(self.base)
+        for idx, value in cbpi.cache["actors"].iteritems():
+            if idx == int(self.dependency):
+                if (value.state == 0) & (self.dependency_type == "Restriction"):
+                    self.api.switch_actor_on(int(self.base), power=power)
+                elif (value.state == 1) & (self.dependency_type == "Prerequisite"):
+                    self.api.switch_actor_on(int(self.base), power=power)
+                else:
+                    self.api.notify(headline="Could not power %s on" % (base_name), message="This is due to the current power state of it's dependency, %s" % (dependency_name), timeout=None, type="danger")
+                    raise UserWarning("Powering on of an actor has been prevented by an actor dependency")
+
+# @cbpi.actor
+# class Actor_with_Prerequisite(ActorBase):
+#     base = Property.Actor("Base Actor", description="Select the actor you would like to add a dependency to")
+#     dependency = Property.Actor("Actor Prerequisite", description="Select the actor that will allow the base actor to become active, when it itself is active")
