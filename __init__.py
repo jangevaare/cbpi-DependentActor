6e7212fe524ebe8292d433f5a3a4b8dd7d238d8e
Justin Angevaare <jangevaa@uoguelph.ca>
2018-02-23 19:32:42 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -15,7 +15,6 @@ class DependentActor(ActorBase):
         for idx, value in cbpi.cache["actors"].iteritems():
             if idx == int(self.base):
                 value.hide = True
-
         # Synchronize current power setting with the Base Actor
         # TODO
 
@@ -26,14 +25,10 @@ class DependentActor(ActorBase):
         self.api.switch_actor_off(int(self.base))
 
     def on(self, power=None):
-        base_name = ""
         dependency_name = ""
         for idx, value in cbpi.cache["actors"].iteritems():
-            if idx == int(self.base):
-                base_name = value.name
             if idx == int(self.dependency):
                 dependency_name = value.name
-
         for idx, value in cbpi.cache["actors"].iteritems():
             if idx == int(self.dependency):
                 if (value.state == 0) & (self.dependency_type == "Restriction"):
@@ -41,10 +36,5 @@ class DependentActor(ActorBase):
                 elif (value.state == 1) & (self.dependency_type == "Prerequisite"):
                     self.api.switch_actor_on(int(self.base), power=power)
                 else:
-                    self.api.notify(headline="Could not power %s on" % (base_name), message="This is due to the current power state of it's dependency, %s" % (dependency_name), timeout=None, type="danger")
-                    raise UserWarning("Powering on of an actor has been prevented by an actor dependency")
-
-# @cbpi.actor
-# class Actor_with_Prerequisite(ActorBase):
-#     base = Property.Actor("Base Actor", description="Select the actor you would like to add a dependency to")
-#     dependency = Property.Actor("Actor Prerequisite", description="Select the actor that will allow the base actor to become active, when it itself is active")
+                    self.api.notify(headline="Powering of actor prevented", message="This is due to the current power state of it's dependency, %s" % (dependency_name), timeout=None, type="danger")
+                    raise UserWarning("Powering of actor prevented by the state of it's dependency")
